bcd4cc884cd90609b082e047804832ad5d93eab2
tremarkley <tremarkley@gmail.com>
2018-01-24 16:34:45 -0800

    Implemented begin of findNRookssolution

--- src/solvers.js
+++ src/solvers.js
@@ -16,10 +16,47 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution; //fixme
+  var board = new Board({n: n});
+  console.log('board-n: ', board.get('n'));
+  console.log(`boardrows ${board.rows()}`);
+  var rooks = 0;
+  var innerFunction = function(board, startingRow, startingColumn) {
+    for (var i = startingRow; i < board.get('n'); i++) {
+      for (var j = startingColumn; j < board.get('n'); j++) {
+        board.togglePiece(i, j);
+        if (!board.hasAnyRooksConflicts()) {
+          rooks++;
+          //check how many pieces are on board, if piece = n we return board
+          if (rooks === n) {
+            return board;
+          }
+          //pass in next available slot and the same board
+          if ((j === board.get('n') - 1) && (i < board.get('n') - 1)) {
+            innerFunction(board, i + 1, 0);
+          } else if (j === board.get('n') - 1 && i === board.get('n') - 1) {
+            return;
+          } else {
+            innerFunction(board, i, j + 1);
+          }         
+            
+        } else {
+          //if there is a conflict untoggle piece
+          board.togglePiece(i, j);
+        }
+      }
+    }
+  };
+  solution = innerFunction(board, 0, 0);
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
+    
+  
+  
+  
+  
+  
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
